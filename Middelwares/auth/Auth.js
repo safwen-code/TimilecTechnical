2b22d772501f79d82156b5b25b36490d08502f10
safwen-code <safwendjebbi1234@gmail.com>
2021-10-17 02:24:49 +0100

    create auth middelware and verify the token

---
+++ Middelwares/auth/Auth.js
@@ -0,0 +1,20 @@
+const config =require('config')
+const jwt =require("jsonwebtoken")
+const safwenToken = config.get('safwenToken')
+function auth(req,res,next){
+    // get Token from header
+    const token =req.header('x-auth-token')
+    //check for token
+    if(!token) return res.status(401).json({msg:"no token authorization false"})
+    try {
+    //verifiy token
+    const decoded =jwt.verify(token, safwenToken)
+    //add user from payload
+    req.user=decoded.user
+    next()
+    } catch (e) {
+        res.status(400).json({msg:'token is not valid'})
+    }
+    
+}
+module.exports= auth
