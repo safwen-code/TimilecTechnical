9fce887d863aadf929d000cb758a078013d6c659
safwen-code <safwendjebbi1234@gmail.com>
2021-10-17 02:40:46 +0100

    create contact model

---
+++ models/Contact.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+
+const contactSchema = new schema({
+  user: {
+    type: schema.Types.ObjectId,
+    ref: "users",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+  },
+  type: {
+    type: String,
+    default: "personal",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Contact = mongoose.model("contact", contactSchema);
